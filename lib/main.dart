5389fe74f970457379a09f05bea253e0fa3784e2
uemirhanselim <uemirhanselim@gmail.com>
2023-03-03 09:21:41 +0300

    some deletes

--- lib/main.dart
+++ lib/main.dart
@@ -12,23 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Animations',
-      theme: ThemeData(
-          fontFamily: "Tilt Warp",
-          appBarTheme: AppBarTheme(color: Colors.purple.shade200),
-          textTheme:
-              const TextTheme(headline5: TextStyle(color: Colors.purple)),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                  minimumSize: const MaterialStatePropertyAll(Size(200, 60)),
-                  backgroundColor: MaterialStateColor.resolveWith(
-                      (states) => Colors.purple.shade200),
-                  shape: const MaterialStatePropertyAll(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)))))),
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              backgroundColor: Colors.purple)),
-      home: const ThemeView(),
+      home: ThemeView(),
     );
   }
 }
